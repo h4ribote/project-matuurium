0466a9b7b07a812be72e36a58061f0c5931babcf
haribote <116637919+h4ribote@users.noreply.github.com>
2023-08-13 18:13:44 +0900

    ライセンスに関する記述を追加

--- main.py
+++ main.py
@@ -109,3 +109,23 @@ async def order_list_command(interaction: discord.Interaction):
 
 
 client.run(TOKEN)
+
+"""
+This software and its source code are provided "as is," without warranty of any kind, express or implied. The author of this software shall not be held liable for any damages arising from the use of this software.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to use the Software for non-commercial purposes only, including the rights to use, copy, modify, merge, publish, and distribute the Software, provided that the following conditions are met:
+
+1. The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+2. Any use of the Software, in whole or in part, for the purpose of creating, operating, or maintaining a bot (automated software application) that interfaces with third-party platforms, services, or APIs (Application Programming Interfaces) as a bot user is strictly prohibited without explicit written permission from the author.
+
+3. Redistributions of the Software, in whole or in part, must retain the above copyright notice, this permission notice, and the disclaimer contained herein.
+
+For the avoidance of doubt, creating, operating, or maintaining a bot that interfaces with third-party platforms, services, or APIs as a bot user requires obtaining a separate, explicit written permission from the author.
+
+For commercial use, please contact h4ribote.
+
+h4ribote's
+email: tyche.d0000@gmail.com
+discord: h4ribote#0
+"""
